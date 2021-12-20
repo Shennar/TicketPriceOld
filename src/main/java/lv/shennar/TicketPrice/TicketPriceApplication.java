92d9d36c0fba73aa0f1ae03be4462017e9668198
Shennar <vlad@sal.lv>
2021-12-20 20:06:30 +0200

    Initial commit

---
+++ src/main/java/lv/shennar/TicketPrice/TicketPriceApplication.java
@@ -0,0 +1,13 @@
+package lv.shennar.TicketPrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketPriceApplication.class, args);
+	}
+
+}
